c62153fb436f8328b5c258c6c270f229c9dc9a3e
Dan Walsh <danwalshnh@gmail.com>
2019-02-22 09:35:49 -0500

    Initial Calculator Commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    def multiply(x,y):return x*y
+    def divide(x,y):return x/y
+    def add(x,y): return x+y
+    def substract(x,y): return x-y
+ 
+x=Calculator.multiply(25,2)
+x    
