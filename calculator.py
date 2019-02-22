c032aad6e1d6604f767fac3fe6e24dc1ab785f65
Dan Walsh <danwalshnh@gmail.com>
2019-02-22 09:43:04 -0500

    Calculator with Linear Equation Solver Class

--- calculator.py
+++ calculator.py
@@ -5,5 +5,15 @@ class Calculator:
     def substract(x,y): return x-y
     def power(x,y): return x**y
     def root(x): return x**(1/2)
-x=Calculator.power(25,2)
+class LinearEq:
+    def slope(x,y,b):
+	return:(y-b)/x
+    def x(m,y,b):
+	return:(y-b)/m
+    def y(m,x,b)
+	return:m*x+b
+    def b(m,x,y):
+	return: y-m*x
+
+x=LinearEq.slope(10,20,3)
 x    
